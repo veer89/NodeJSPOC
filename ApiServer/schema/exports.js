fa5f7a8ba398f868f698470ca0339784cc8de6fb
Priya Agarwal <p.agarwal@globallogic.com>
2014-04-02 11:17:17 +0530

    Address schema along with its CRUD operations

--- ApiServer/schema/exports.js
+++ ApiServer/schema/exports.js
@@ -2,3 +2,4 @@ exports.userModel = require('./user.js').userModel;
 exports.socialModel = require('./social.js').socialModel;
 exports.projectModel = require('./project.js').projectModel;
 exports.loginModel = require('./login.js').loginModel;
+exports.addressModel = require('./address.js').addressModel;
