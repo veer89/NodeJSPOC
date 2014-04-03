68f4b03534594d47b6195e34b6358beaea06a1e6
parasmishra <paras.mishra@globallogic.com>
2014-04-03 10:16:01 +0530

    resolved conflicts
    resolved conflicts

--- ApiServer/schema/exports.js
+++ ApiServer/schema/exports.js
@@ -2,8 +2,6 @@ exports.userModel = require('./user.js').userModel;
 exports.socialModel = require('./social.js').socialModel;
 exports.projectModel = require('./project.js').projectModel;
 exports.loginModel = require('./login.js').loginModel;
-<<<<<<< HEAD
 exports.picModel = require('./pic.js').picModel;
-=======
 exports.addressModel = require('./address.js').addressModel;
->>>>>>> fa5f7a8ba398f868f698470ca0339784cc8de6fb
+
