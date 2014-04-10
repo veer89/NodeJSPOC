d1a0689bdd543b483ee6aceac5ca1082b171acf4
mainak <mainak@mainak-VirtualBox>
2014-04-10 23:18:58 +0530

    added all routes to endpoint.json, changed helper request function

--- NodeJsClient/public/utils/helper.js
+++ NodeJsClient/public/utils/helper.js
@@ -2,23 +2,23 @@ var rest = require('restler');
 var helper = {};
 
 helper = {
-	sendRequest : function(endpoint ,data, user_id, callback){
-		var requestUrl = global._BASEURL + endpoint.end_point 
-						+ '?appKey=' + global._APPKEY;
+	sendRequest : function(endpoint ,data, user_id, params, callback){
+		var requestUrl = global._BASEURL + endpoint.end_point ;
+		if(params && param.length > 0){
+			for(var i = 0;i < params.length; i++){
+				requestUrl += '/' + params[i];
+			}
+		}
+		requestUrl += '?appKey=' + global._APPKEY;
 		if(user_id) {
 			requestUrl += '&user_id=' + user_id;
 		}
-		if(endpoint.method_type == 'POST'){
-			rest.post(requestUrl, {
-				data : data,
-			}).once('complete', function(data, response) {
-				callback(data);
-			});
-		} else if(endpoint.method_type == 'GET'){
-			rest.get(requestUrl).once('complete', function(data, response) {
-				callback(data);
-			});
-		}
+		rest.request(requestUrl, {
+			method : endpoint.method_type,
+			data : data
+		}).once('complete', function(data, response) {
+			callback(JSON.parse(data));
+		});
 	}
 };
 
--- NodeJsClient/routes/index.js
+++ NodeJsClient/routes/index.js
@@ -7,23 +7,24 @@ index.getIndex = function(req, res, next){
 	res.render('index', { title: 'Express' });
 };
 
-index.signup = function(req, res, next){
-	var data = {
-		password : req.body.password,
-		empId : req.body.username,
-		emailId : req.body.email
-	};
-	helper.sendRequest(endPoints.users.create, data, null, function(result) {
-		if (result && result.meta) {
+index = {
+		signup = function(req, res, next){
+			var data = {
+				password : req.body.password,
+				empId : req.body.username,
+				emailId : req.body.email
+			};
+			helper.sendRequest(endPoints.users.create, data, null, null, function(result) {
+				if (result && result.meta) {
+					if (result.meta.status == '200') {
+						res.send('Created');
+					} else {
+						res.send('Error Occured');
+					}
 
-			if (result.meta.status == '200') {
-				res.send('Created');
-			} else {
-				res.send('Error Occured');
-			}
-
+				}
+			});
+			
 		}
-	});
-	
 }
 module.exports = index;
