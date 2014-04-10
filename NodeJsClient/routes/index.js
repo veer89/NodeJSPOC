c9fff2a4d26239d16994be7f3bba04fde63afdb7
mainak <mainak@mainak-VirtualBox>
2014-04-11 00:04:47 +0530

    signup page

--- NodeJsClient/routes/index.js
+++ NodeJsClient/routes/index.js
@@ -3,16 +3,20 @@ var helper = require('../public/utils/helper.js');
 var endPoints = require('../endPoints.json');
 
 var index = {};
-index.getIndex = function(req, res, next){   
-	res.render('index', { title: 'Express' });
-};
 
 index = {
-		signup = function(req, res, next){
+		getIndex : function(req, res, next){   
+			res.render('index', { title: 'Express' });
+		},
+		signup : function(req, res, next){
 			var data = {
 				password : req.body.password,
 				empId : req.body.username,
-				emailId : req.body.email
+				emailId : req.body.email,
+				first_name: req.body.first_name,
+		        last_name: req.body.last_name,
+		        phone_number: req.body.phone_number,
+		        designation: req.body.designation
 			};
 			helper.sendRequest(endPoints.users.create, data, null, null, function(result) {
 				if (result && result.meta) {
