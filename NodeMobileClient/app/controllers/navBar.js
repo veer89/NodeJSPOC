f0b8a75176878a0a83647a65c604198b06af38ce
Veer Jain <veer.jain@globallogic.com>
2014-04-16 16:33:19 +0530

    Added edit profile

--- NodeMobileClient/app/controllers/navBar.js
+++ NodeMobileClient/app/controllers/navBar.js
@@ -1,26 +1,25 @@
 var args = arguments[0] || {};
 
-
-// if back Btn param available, set it
-if(args.backBtn){
+if(args){
 	$.backBtn.visible = args.backBtn;
+	$.settings.visible = args.settings;
 }
 
 // if title param available, set it
 if(args.title){
 	$.header.text = args.title;
-
+}
 
 /**
  * ************************************
  * EVENT LISTENERS
  * ************************************
  */
-
 function closeEdit(){
-	
+	Alloy.Globals.mainWindow.remove(Alloy.Globals.mainWindow.children[Alloy.Globals.mainWindow.children.length-1]);
 }
 
-function editProfile(){	
-
+function editProfile(){
+	var editProfile = Alloy.createController('editProfile').getView();
+	Alloy.Globals.mainWindow.add(editProfile);
 }
